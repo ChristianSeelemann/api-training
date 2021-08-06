0852f12f1742dd52d1dd771b243d323afe532c77
ChristianSeelemann <cseelemann@gmx.de>
2021-08-06 13:31:57 +0200

    🚧 First layout for the game card

--- src/components/gameList/gameList.ts
+++ src/components/gameList/gameList.ts
@@ -2,9 +2,53 @@ import styles from '../gameList/gameList.module.css';
 import { createElement } from '../../utils/functions/createElement';
 
 export function gameList(): HTMLElement {
-  const gameListOutput = createElement('div', {
-    innerText: 'Hello',
+  const descriptionInput =
+    'A browser-based 3D space-combat MMO with a massive playerbase! A browser-based 3D space-combat MMO with a massive playerbase! A browser-based 3D space-combat MMO with a massive playerbase!';
+  const descriptionOutput = descriptionInput.slice(0, 80) + '...';
+
+  const gameCard = createElement('article', {
+    className: styles.card,
+    childElements: [
+      createElement('div', {
+        className: styles.top,
+        childElements: [
+          createElement('img', {
+            className: styles.img,
+            src: 'https://www.freetogame.com/g/1/thumbnail.jpg',
+          }),
+          createElement('div', {
+            className: styles.genre,
+            innerText: 'MMORPG',
+          }),
+        ],
+      }),
+      createElement('section', {
+        className: styles.info,
+        childElements: [
+          createElement('h2', {
+            className: styles.title,
+            innerText: 'Dark Orbit Reloaded Dark Orbit Reloaded',
+          }),
+          createElement('p', {
+            className: styles.description,
+            innerText: descriptionOutput,
+          }),
+          createElement('section', {
+            className: styles.additionals,
+            childElements: [
+              createElement('div', {
+                className: styles.publisher,
+                innerText: 'Bigpoint | Digital Extremes',
+              }),
+              createElement('div', {
+                innerHTML: '<i class="fab fa-xbox"></i>',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   });
 
-  return gameListOutput;
+  return gameCard;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { createElement } from './utils/functions/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const output = createElement('main', {
-  childElements: [gameList()],
+const output = createElement('div', {
+  className: 'page',
+  childElements: [
+    createElement('main', {
+      childElements: [
+        createElement('section', {
+          className: 'gamelist-container',
+          childElements: [gameList(), gameList(), gameList(), gameList()],
+        }),
+      ],
+    }),
+  ],
 });
 
 if (app !== null) {
