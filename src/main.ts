a12195473974b45f40991b161f86ad84b71985bc
ChristianSeelemann <cseelemann@gmx.de>
2021-08-06 23:36:35 +0200

    🗑️ Cleanup main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,6 @@ import { createElement } from './utils/functions/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-/* const data = await fetchAPI('');
-const smallData = data.splice(0, 20);
-
-const gameContainer = createElement('main', {
-  className: 'gamelist-container',
-  childElements: smallData.map((single: Games) => gameList(single)),
-}); */
-
 const gameContainer = createElement('main', {
   className: 'gamelist-container',
 });
